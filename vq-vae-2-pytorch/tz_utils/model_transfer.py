d92e1df0b1086f93a6d0d454c1c7a6e108f67e55
Garfield-Finch <tuzhitz@163.com>
2020-02-27 21:54:36 +0800

    init model_transfer.py

--- vq-vae-2-pytorch/tz_utils/model_transfer.py
+++ vq-vae-2-pytorch/tz_utils/model_transfer.py
@@ -188,14 +188,20 @@ class VQVAE(nn.Module):
             stride=4,
         )
 
-    def forward(self, input):
-        quant_t, quant_b, diff, _, _ = self.encode(input)
-        dec = self.decode(quant_t, quant_b)
-
-        # return dec, diff
-
-        # replace previous return
-        return dec, diff, quant_t, quant_b
+    def forward(self, input, mode='GENERATE'):
+        if mode == 'GENERATE':
+            quant_t, quant_b, diff, _, _ = self.encode(input)
+            dec = self.decode(quant_t, quant_b)
+            # return dec, diff
+            # replace previous return
+            return dec, diff, quant_t, quant_b
+        elif mode == 'TRANSFER':
+            quant_t, quant_b = input
+            dec = self.decode(quant_t, quant_b)
+            return dec
+        else:
+            print('mode type ERROR')
+            exit()
 
     def encode(self, input):
         enc_b = self.enc_b(input)
@@ -232,3 +238,42 @@ class VQVAE(nn.Module):
         dec = self.decode(quant_t, quant_b)
 
         return dec
+
+
+class TransferModel(nn.Module):
+    def __init__(
+        self,
+        in_channel=3,
+        channel=128,
+        n_res_block=2,
+        n_res_channel=32,
+        embed_dim=64,
+        n_embed=512,
+        decay=0.99,
+    ):
+        super().__init__()
+
+        # self.enc_b = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
+        # self.enc_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
+        # self.quantize_conv_t = nn.Conv2d(channel, embed_dim, 1)
+        # self.quantize_t = Quantize(embed_dim, n_embed)
+        # self.dec_t = Decoder(
+        #     embed_dim, embed_dim, channel, n_res_block, n_res_channel, stride=2
+        # )
+        self.quantize_conv_b = nn.Conv2d(embed_dim + channel, embed_dim, 1)
+        # self.quantize_b = Quantize(embed_dim, n_embed)
+        # self.upsample_t = nn.ConvTranspose2d(
+        #     embed_dim, embed_dim, 4, stride=2, padding=1
+        # )
+        # self.dec = Decoder(
+        #     embed_dim + embed_dim,
+        #     in_channel,
+        #     channel,
+        #     n_res_block,
+        #     n_res_channel,
+        #     stride=4,
+        # )
+
+    def forward(self, quant_t, quant_b):
+
+        return quant_t, quant_b
