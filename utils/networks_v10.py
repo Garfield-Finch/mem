16e323c942110a64a00cceafd10e66e86ebbf209
Garfield-Finch <tuzhitz@163.com>
2020-03-24 19:13:34 +0800

    design VQVAE for warping separate pose VQVAE and appearance VQVAE

--- utils/networks_v10.py
+++ utils/networks_v10.py
@@ -181,15 +181,10 @@ class VQVAE(nn.Module):
         self.quantize_conv_b = nn.Conv2d(embed_dim + channel, embed_dim, 1)
         self.quantize_b = Quantize(embed_dim, n_embed)
 
-    def forward(self, input, mode='Default'):
-        if mode == 'Default':
-            quant_t, quant_b, diff, _, _ = self.encode(input)
-            dec = self.decode(quant_t, quant_b)
-            return dec, diff
-        elif mode == 'TRANSFER':
-            quant_t, quant_b, diff, _, _ = self.encode(input)
-            dec = self.decode(quant_t, quant_b)
-            return dec, diff
+    def forward(self, input):
+        quant_t, quant_b, diff, _, _ = self.encode(input)
+        dec = self.decode(quant_t, quant_b)
+        return dec, diff, quant_t, quant_b
 
     def encode(self, input):
         enc_b = self.enc_b(input)
@@ -221,6 +216,96 @@ class VQVAE(nn.Module):
         quant_t = self.quantize_t.embed_code(code_t)
         quant_t = quant_t.permute(0, 3, 1, 2)
         quant_b = self.quantize_b.embed_code(code_b)
+        quant_b = quant_b.permute(0, 3, 1, 2)
+
+        dec = self.decode(quant_t, quant_b)
+
+        return dec
+
+
+class AppVQVAE(nn.Module):
+    def __init__(
+        self,
+        in_channel=3,
+        channel=128,
+        n_res_block=2,
+        n_res_channel=32,
+        embed_dim=64,
+        n_embed=512,
+        decay=0.99,
+    ):
+        super().__init__()
+
+        self.enc_b = Encoder(in_channel, channel, n_res_block, n_res_channel, stride=4)
+        self.enc_t = Encoder(channel, channel, n_res_block, n_res_channel, stride=2)
+        self.quantize_conv_t = nn.Conv2d(channel, embed_dim, 1)
+        self.quantize_t = Quantize(embed_dim, n_embed)
+        self.dec_t = Decoder(
+            embed_dim, embed_dim, channel, n_res_block, n_res_channel, stride=2
+        )
+        self.quantize_conv_b = nn.Conv2d(embed_dim + channel, embed_dim, 1)
+        self.quantize_b = Quantize(embed_dim, n_embed)
+
+    def forward(self, input, mode='Default'):
+        if mode == 'Default':
+            quant_t, quant_b, diff, quant_t_prev, quant_b_prev, _, _ = self.encode(input)
+            dec = self.decode(quant_t, quant_b)
+            return dec, quant_t, quant_b,
+        elif mode == 'TRANSFER':
+            quant_t, quant_b = input
+            self.seq2quant_decode(quant_t, quant_b)
+            # dec = self.decode(quant_t, quant_b)
+            # return dec
+        else:
+            print('mode type ERROR')
+            exit()
+
+    def encode(self, input):
+        enc_b = self.enc_b(input)
+        enc_t = self.enc_t(enc_b)
+
+        quant_t = self.quantize_conv_t(enc_t).permute(0, 2, 3, 1)
+        # TODO to check whether should add a clone function somehow
+        quant_t_prev = quant_t
+        quant_t, diff_t, id_t = self.quantize_t(quant_t)
+        quant_t = quant_t.permute(0, 3, 1, 2)
+        diff_t = diff_t.unsqueeze(0)
+
+        dec_t = self.dec_t(quant_t)
+        enc_b = torch.cat([dec_t, enc_b], 1)
+
+        quant_b = self.quantize_conv_b(enc_b).permute(0, 2, 3, 1)
+        quant_b_prev = quant_b
+        quant_b, diff_b, id_b = self.quantize_b(quant_b)
+        quant_b = quant_b.permute(0, 3, 1, 2)
+        diff_b = diff_b.unsqueeze(0)
+
+        return quant_t, quant_b, diff_t + diff_b, quant_t_prev, quant_b_prev, id_t, id_b
+
+    def decode(self, quant_t, quant_b):
+        upsample_t = self.upsample_t(quant_t)
+        quant = torch.cat([upsample_t, quant_b], 1)
+        dec = self.dec(quant)
+
+        return dec
+
+    def decode_code(self, code_t, code_b):
+        quant_t = self.quantize_t.embed_code(code_t)
+        quant_t = quant_t.permute(0, 3, 1, 2)
+        quant_b = self.quantize_b.embed_code(code_b)
+        quant_b = quant_b.permute(0, 3, 1, 2)
+
+        dec = self.decode(quant_t, quant_b)
+
+        return dec
+
+    def seq2quant_decode(self, quant_t, quant_b):
+        quant_t = quant_t.permute(0, 2, 3, 1)
+        quant_t, _, _ = self.quantize_t(quant_t)
+        quant_t = quant_t.permute(0, 3, 1, 2)
+
+        quant_b = quant_b.permute(0, 2, 3, 1)
+        quant_b, _, _ = self.quantize_t(quant_b)
         quant_b = quant_b.permute(0, 3, 1, 2)
 
         dec = self.decode(quant_t, quant_b)
