33eec20e469b017bdc6ec63c8c61d8f48aed457c
Garfield-Finch <tuzhitz@163.com>
2020-03-25 16:17:06 +0800

    add tensor clone

--- utils/networks_v10.py
+++ utils/networks_v10.py
@@ -271,8 +271,8 @@ class AppVQVAE(nn.Module):
             dec = self.decode(quant_t, quant_b)
             return dec, diff, quant_t, quant_b
         elif mode == 'TRANSFER':
-            quant_t, quant_b = input
-            self.seq2quant_decode(quant_t, quant_b)
+            (quant_t, quant_b) = input
+            return self.seq2quant_decode(quant_t, quant_b)
             # dec = self.decode(quant_t, quant_b)
             # return dec
         else:
@@ -284,8 +284,8 @@ class AppVQVAE(nn.Module):
         enc_t = self.enc_t(enc_b)
 
         quant_t = self.quantize_conv_t(enc_t).permute(0, 2, 3, 1)
-        # TODO to check whether should add a clone function somehow
-        quant_t_prev = quant_t
+        # add clone
+        quant_t_prev = quant_t.clone()
         quant_t, diff_t, id_t = self.quantize_t(quant_t)
         quant_t = quant_t.permute(0, 3, 1, 2)
         diff_t = diff_t.unsqueeze(0)
@@ -294,7 +294,8 @@ class AppVQVAE(nn.Module):
         enc_b = torch.cat([dec_t, enc_b], 1)
 
         quant_b = self.quantize_conv_b(enc_b).permute(0, 2, 3, 1)
-        quant_b_prev = quant_b
+        # add clone
+        quant_b_prev = quant_b.clone()
         quant_b, diff_b, id_b = self.quantize_b(quant_b)
         quant_b = quant_b.permute(0, 3, 1, 2)
         diff_b = diff_b.unsqueeze(0)
