7074226388aa4d310e33d1db6915b018b7548315
Garfield-Finch <tuzhitz@163.com>
2020-04-22 16:31:18 +0800

    separate two tz_generator

--- SPADE/models/networks/tz_generator_v01.py
+++ SPADE/models/networks/tz_generator_v01.py
@@ -35,8 +35,8 @@ class SPADEGenerator(BaseNetwork):
         else:
             # Otherwise, we make the network deterministic by starting with
             # downsampled segmentation map instead of random z
-            self.fc = nn.Conv2d(self.opt.semantic_nc, 16 * nf, 3, padding=1)
-            # self.fc = nn.Conv2d(256, 16 * nf, 3, padding=1)
+            # self.fc = nn.Conv2d(self.opt.semantic_nc, 16 * nf, 3, padding=1)
+            self.fc = nn.Conv2d(256, 16 * nf, 3, padding=1)
 
         self.head_0 = SPADEResnetBlock(16 * nf, 16 * nf, opt)
 
